c1da06c99a23b72d109677b0d811384fb82fb310
Arthur Ruschel <aruschelbr@gmail.com>
2026-04-20 04:56:38 -0300

    Integrando o firebase.

---
+++ assets/js/firebase.env.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyD7c57FzA7FsUo0xISFSvzCyCyQxaNLBmA",
+    authDomain: "locacao-equipamentos.firebaseapp.com",
+    databaseURL: "https://locacao-equipamentos-default-rtdb.firebaseio.com",
+    projectId: "locacao-equipamentos",
+    storageBucket: "locacao-equipamentos.firebasestorage.app",
+    messagingSenderId: "222522636818",
+    appId: "1:222522636818:web:4a98e9577b9847ab9276cb"
+};
